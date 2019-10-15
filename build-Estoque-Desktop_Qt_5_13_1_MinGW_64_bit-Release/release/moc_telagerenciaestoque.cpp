68482f3b6b72ffedb918c33ca1ea8efda2281f4f
masumotion302 <masumotovic@gmail.com>
2019-10-15 18:28:48 -0300

    part10

---
+++ build-Estoque-Desktop_Qt_5_13_1_MinGW_64_bit-Release/release/moc_telagerenciaestoque.cpp
@@ -0,0 +1,177 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'telagerenciaestoque.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.13.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../Estoque/telagerenciaestoque.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'telagerenciaestoque.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.13.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_TelaGerenciaEstoque_t {
+    QByteArrayData data[17];
+    char stringdata0[429];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_TelaGerenciaEstoque_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_TelaGerenciaEstoque_t qt_meta_stringdata_TelaGerenciaEstoque = {
+    {
+QT_MOC_LITERAL(0, 0, 19), // "TelaGerenciaEstoque"
+QT_MOC_LITERAL(1, 20, 30), // "on_btnCadastrarProduto_clicked"
+QT_MOC_LITERAL(2, 51, 0), // ""
+QT_MOC_LITERAL(3, 52, 25), // "on_btnEncontrarId_clicked"
+QT_MOC_LITERAL(4, 78, 39), // "on_spnQuantidadeProduto_editi..."
+QT_MOC_LITERAL(5, 118, 31), // "on_spnIdProduto_editingFinished"
+QT_MOC_LITERAL(6, 150, 33), // "on_txtNomeProduto_editingFini..."
+QT_MOC_LITERAL(7, 184, 34), // "on_spnPrecoProduto_editingFin..."
+QT_MOC_LITERAL(8, 219, 23), // "on_btnPesquisar_clicked"
+QT_MOC_LITERAL(9, 243, 37), // "on_tabCadastrarProdutos_tabBa..."
+QT_MOC_LITERAL(10, 281, 5), // "index"
+QT_MOC_LITERAL(11, 287, 21), // "on_btnExcluir_clicked"
+QT_MOC_LITERAL(12, 309, 33), // "on_btnListarTodosProdutos_cli..."
+QT_MOC_LITERAL(13, 343, 40), // "on_tabGerenciadorDeEstoque_ta..."
+QT_MOC_LITERAL(14, 384, 33), // "on_twListaDeCompras_cellActiv..."
+QT_MOC_LITERAL(15, 418, 3), // "row"
+QT_MOC_LITERAL(16, 422, 6) // "column"
+
+    },
+    "TelaGerenciaEstoque\0on_btnCadastrarProduto_clicked\0"
+    "\0on_btnEncontrarId_clicked\0"
+    "on_spnQuantidadeProduto_editingFinished\0"
+    "on_spnIdProduto_editingFinished\0"
+    "on_txtNomeProduto_editingFinished\0"
+    "on_spnPrecoProduto_editingFinished\0"
+    "on_btnPesquisar_clicked\0"
+    "on_tabCadastrarProdutos_tabBarClicked\0"
+    "index\0on_btnExcluir_clicked\0"
+    "on_btnListarTodosProdutos_clicked\0"
+    "on_tabGerenciadorDeEstoque_tabBarClicked\0"
+    "on_twListaDeCompras_cellActivated\0row\0"
+    "column"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TelaGerenciaEstoque[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      12,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    0,   75,    2, 0x08 /* Private */,
+       4,    0,   76,    2, 0x08 /* Private */,
+       5,    0,   77,    2, 0x08 /* Private */,
+       6,    0,   78,    2, 0x08 /* Private */,
+       7,    0,   79,    2, 0x08 /* Private */,
+       8,    0,   80,    2, 0x08 /* Private */,
+       9,    1,   81,    2, 0x08 /* Private */,
+      11,    0,   84,    2, 0x08 /* Private */,
+      12,    0,   85,    2, 0x08 /* Private */,
+      13,    1,   86,    2, 0x08 /* Private */,
+      14,    2,   89,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int, QMetaType::Int,   15,   16,
+
+       0        // eod
+};
+
+void TelaGerenciaEstoque::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<TelaGerenciaEstoque *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_btnCadastrarProduto_clicked(); break;
+        case 1: _t->on_btnEncontrarId_clicked(); break;
+        case 2: _t->on_spnQuantidadeProduto_editingFinished(); break;
+        case 3: _t->on_spnIdProduto_editingFinished(); break;
+        case 4: _t->on_txtNomeProduto_editingFinished(); break;
+        case 5: _t->on_spnPrecoProduto_editingFinished(); break;
+        case 6: _t->on_btnPesquisar_clicked(); break;
+        case 7: _t->on_tabCadastrarProdutos_tabBarClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->on_btnExcluir_clicked(); break;
+        case 9: _t->on_btnListarTodosProdutos_clicked(); break;
+        case 10: _t->on_tabGerenciadorDeEstoque_tabBarClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->on_twListaDeCompras_cellActivated((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject TelaGerenciaEstoque::staticMetaObject = { {
+    &QDialog::staticMetaObject,
+    qt_meta_stringdata_TelaGerenciaEstoque.data,
+    qt_meta_data_TelaGerenciaEstoque,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *TelaGerenciaEstoque::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TelaGerenciaEstoque::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_TelaGerenciaEstoque.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int TelaGerenciaEstoque::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 12)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 12;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 12)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 12;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
